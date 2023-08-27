42e98d42151437dd0d9b6312435f1ae61e0e508d
aryash19 <Aryashchaudhary12341234@gmail.com>
2023-08-27 23:24:19 +0530

    new

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Dimensions} from 'react-native';
 import styles from './styles';
 import Svg, {Image} from 'react-native-svg'
 
 export default function App() {
+  const {height, width}=Dimensions.get('window');
   return (
     <View style={styles.container}>
-      <Svg>
+      <Svg height={height} width={width}>
+        <Image href={require('./assets/login-background.jpg') } height={height} width={width}
+        preserveAspectRatio="xMidYMid slice"/>
         
       </Svg>
+      <View>
+        <View>
+          <Text>Log In </Text>
+        </View>
+      </View>
       
     </View>
   );
